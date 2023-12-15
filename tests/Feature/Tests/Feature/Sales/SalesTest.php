1e2ac206825e8554b9dc69823fc1e4d98eb63cef
Paul Caligari <pcaligari68@gmail.com>
2023-12-15 14:39:15 +0000

    test: write failing test prior to adding functionality

---
+++ tests/Feature/Tests/Feature/Sales/SalesTest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Tests\Feature\Sales;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class SalesTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_sales_page_can_be_rendered()
+    {
+        $user = User::factory()->create();
+
+        $response = $this->actingAs($user)->get('/sales');
+
+        $response->assertStatus(200);
+    }
+
+    public function test_sales_can_be_recorded()
+    {
+        $user = User::factory()->create();
+
+        $response = $this->actingAs($user)->post('/sales', [
+            'quantity' => 10,
+            'unit-cost' => 20.4
+        ]);
+
+        $response->assertSessionHasNoErrors();
+    }
+
+}
