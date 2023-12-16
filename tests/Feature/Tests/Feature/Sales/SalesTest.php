d45bce6575e96c4f470c8a4b7a8304522915594b
Paul Caligari <pcaligari68@gmail.com>
2023-12-16 10:59:12 +0000

    test: refactor sales tests to look for the new sales form

--- tests/Feature/Tests/Feature/Sales/SalesTest.php
+++ tests/Feature/Tests/Feature/Sales/SalesTest.php
@@ -4,22 +4,40 @@ namespace Tests\Feature\Sales;
 
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Testing\TestResponse;
 use Tests\TestCase;
 
 class SalesTest extends TestCase
 {
     use RefreshDatabase;
 
-    public function test_sales_page_can_be_rendered()
+    public function test_sales_page_can_be_rendered() :TestResponse
     {
         $user = User::factory()->create();
 
         $response = $this->actingAs($user)->get('/sales');
 
         $response->assertStatus(200);
+
+        return $response;
     }
 
-    public function test_sales_can_be_recorded()
+    /**
+     * @depends test_sales_page_can_be_rendered
+     */
+    public function test_sale_page_has_sales_form(TestResponse $response) :void
+    {
+        // Not the best test but it'll do to demonstrate the principle..
+        $response->assertSee([
+            'salesForm',
+            'Quantity',
+            'Unit Cost (&pound;)',
+            'Selling Price',
+            'Record Sale'
+        ]);
+    }
+
+    public function test_sales_can_be_recorded() :void
     {
         $user = User::factory()->create();
 
@@ -28,7 +46,6 @@ class SalesTest extends TestCase
             'unit-cost' => 20.4
         ]);
 
-        //$response->dump();
 
         $response->assertSessionHasNoErrors();
     }
